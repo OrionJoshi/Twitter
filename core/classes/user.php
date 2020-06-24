223eb43fe7c4a12fc49e4ef10f93f2829551de2f
khageshwor <khageshwarjoshi03@gmail.com>
2020-06-24 16:23:25 +0545

    Check user valid or not

--- core/classes/user.php
+++ core/classes/user.php
@@ -13,5 +13,22 @@
             $var = stripcslashes($var);
             return $var;
         }
+
+        public function login($email, $password) {
+            $stmt = $this->pdo->prepare("SELECT `user_id` FROM `users` WHERE `email` = :email AND `password` = :pass");
+            $stmt->bindParam(":email", $email);
+            $stmt->bindParam(":pass", md5($password));
+            $stmt->execute();
+
+            $user = $stmt->fetch(PDO::FETCH_OBJ);
+            $count = $stmt->rowCount();
+
+            if($count > 0) {
+                $_SESSION['user_id'] = $user->user_id;
+                header('Location:home.php');
+            } else {
+                return false;
+            }
+        }
     }
 ?>
