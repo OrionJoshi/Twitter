b048e6c17b9e6d091146e1d9b13b7c7eca201069
khageshwor <khageshwarjoshi03@gmail.com>
2020-06-27 16:12:23 +0545

    Fixed issue

--- core/classes/user.php
+++ core/classes/user.php
@@ -61,12 +61,12 @@
 
         public function create($table, $fields = array()) {
             $columns = implode(',', array_keys($fields));
-            $values = ':' .implode(', :', array_keys($fields));
+            $values = ':'.implode(', :', array_keys($fields));
             $sql = "INSERT INTO {$table} ({$columns}) VALUES ({$values})";
 
             if($stmt = $this->pdo->prepare($sql)) {
                 foreach($fields as $key => $data) {
-                    $stmt->bindParam(':'.$key, $data);
+                    $stmt->bindValue(':'.$key, $data);
                 }
                 $stmt->execute();
                 return $this->pdo->lastInsertId();
