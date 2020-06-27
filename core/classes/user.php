4cceecd6cdccb134422547733af722cb00763ee6
khageshwor <khageshwarjoshi03@gmail.com>
2020-06-27 15:21:59 +0545

    Add user create method

--- core/classes/user.php
+++ core/classes/user.php
@@ -45,18 +45,32 @@
             header('Location: ../index.php');
         }
 
-        public function register($email, $screenName, $password) {
-            $stmt = $this->pdo->prepare("INSERT INTO `users` (`email`, `password`, `screenName`, `profileImage`, `profileCover`) VALUES (:email, :pass, :screenName, 'assets/images/defaultProfileImage.png', 'assets/images/defaultCoverImage.png')");
-            $stmt->bindParam(":email", $email);
-            $hash = md5($password);
-            $stmt->bindParam(":pass", $hash);
-            $stmt->bindParam(":screenName", $screenName);
-            $stmt->execute();
+        // public function register($email, $screenName, $password) {
+        //     $stmt = $this->pdo->prepare("INSERT INTO `users` (`email`, `password`, `screenName`, `profileImage`, `profileCover`) VALUES (:email, :pass, :screenName, 'assets/images/defaultProfileImage.png', 'assets/images/defaultCoverImage.png')");
+        //     $stmt->bindParam(":email", $email);
+        //     $hash = md5($password);
+        //     $stmt->bindParam(":pass", $hash);
+        //     $stmt->bindParam(":screenName", $screenName);
+        //     $stmt->execute();
             
 
-            $user_id = $this->pdo->lastInsertId();
-            $_SESSION['user_id'] = $user_id;
+        //     $user_id = $this->pdo->lastInsertId();
+        //     $_SESSION['user_id'] = $user_id;
 
+        // }
+
+        public function create($table, $fields = array()) {
+            $columns = implode(',', array_keys($fields));
+            $values = ':' .implode(', :', array_keys($fields));
+            $sql = "INSERT INTO {$table} ({$columns}) VALUES ({$values})";
+
+            if($stmt = $this->pdo->prepare($sql)) {
+                foreach($fields as $key => $data) {
+                    $stmt->bindParam(':'.$key, $data);
+                }
+                $stmt->execute();
+                return $this->pdo->lastInsertId();
+            }
         }
 
         public function checkEmail($email) {
