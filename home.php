8b40f5d7a407400738e289002a8ae4e8ca2cb222
khageshwor <khageshwarjoshi03@gmail.com>
2020-07-04 13:27:39 +0545

    Add user tweet to database

--- home.php
+++ home.php
@@ -5,6 +5,24 @@
 	if($getFromU->loggedIn() === false) {
 		header('Location: index.php');
 	}
+
+	if(isset($_POST['tweet'])) {
+		$status = $getFromU->checkinput($_POST['status']);
+		$tweetImage = '';
+
+		if(!empty($status) or !empty($_FILES['file']['name'][0])) {
+			if(!empty($_FILES['file']['name'][0])) {
+				$tweetImage = $getFromU_>uploadImage($_FILES['file']);
+			}
+
+			if(strlen($status) > 140) {
+				$error = "The text of your tweet is too long";
+			}
+			$getFromU->create('tweets', array('status' => $status, 'tweetBy' => $user_id, 'tweetImage' => $tweetImage, 'postedOn' => date('Y-m-d H:i:s')));
+		} else {
+			$error = "Type or choose  image to tweet";
+		}
+	}
 ?>
 <!DOCTYPE HTML> 
  <html>
@@ -153,7 +171,7 @@
 						 		<ul>
 						 			<input type="file" name="file" id="file"/>
 						 			<li><label for="file"><i class="fa fa-camera" aria-hidden="true"></i></label>
-						 			<span class="tweet-error"></span>
+						 			<span class="tweet-error"><?php if(isset($error)){ echo $error;} else if(isset($imageError)){ echo $imageError;}  ?></span>
 						 			</li>
 						 		</ul>
 						 	</div>
