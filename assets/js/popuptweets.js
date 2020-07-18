a0248d3d58f4d84a3be7d84b811ac07627fb30ac
khageshwor <khageshwarjoshi03@gmail.com>
2020-07-18 18:34:45 +0545

    Add jquery function to hide tweet popup box

--- assets/js/popuptweets.js
+++ assets/js/popuptweets.js
@@ -3,6 +3,9 @@ $(function(){
         var tweet_id = $(this).data('tweet');
         $.post('http://localhost/twitter/core/ajax/popuptweets.php', {showpopup:tweet_id}, function(data){
             $('.popupTweet').html(data);
+            $('.tweet-show-popup-box-cut').click(function(){
+                $('.tweet-show-popup-wrap').hide();
+            });
         });
     });
 });
