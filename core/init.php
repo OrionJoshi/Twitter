61ef87e60ca2ed44988afb0036b67aa3a3092bf5
khageshwor <khageshwarjoshi03@gmail.com>
2020-08-03 16:02:11 +0545

    Include message class and its object

--- core/init.php
+++ core/init.php
@@ -3,6 +3,7 @@
     include 'classes/user.php';
     include 'classes/tweet.php';
     include 'classes/follow.php';
+    include 'classes/message.php';
 
     global $pdo;
 
@@ -11,6 +12,8 @@
     $getFromU = new User($pdo);
     $getFromT = new Tweet($pdo);
     $getFromF = new Follow($pdo);
+    $getFromM = new Message($pdo);
+
 
     define('BASE_URL', 'http://localhost/Twitter/');
  ?>
